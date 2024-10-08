ade528d5bdadb7993d9133b86fcdfe0441affd57
Luriannys <131465439+Luriannys@users.noreply.github.com>
2024-10-08 13:35:56 -0400

    Semaforo

--- HP vs Dell - Equipo 23/src/Main.java
+++ HP vs Dell - Equipo 23/src/Main.java
@@ -1,3 +1,8 @@
+
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -7,13 +12,79 @@
  *
  * @author Luriannys Junco
  */
-public class Main {
+public class Main extends Thread{
+    
+    private Semaphore semaphore;
+    private int id;
+    public int horas;
+    public int dias;
+    public boolean computadora;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public Main(Semaphore semaforo,int id) {
+        this.semaphore=semaforo;
+        this.id=id;
+        
     }
+    @Override
+    public void run(){
+        try {
+            
+            System.out.println("PM esta llegando");
+            System.out.println("Director esta llegando");
+            semaphore.acquire();
+            while (horas<24){
+                while (horas<17){
+                System.out.println("PM esta trabajando");
+
+                Thread.sleep(3000);
+                System.out.println("PM esta viendo anime");
+                Thread.sleep(1000);
+                horas++;
+                }
+                horas++;
+
+                if (horas >23){
+                    dias++;
+                    horas=0;
+                   //los que tardan 1 dia se suman y ya 
+                   if (dias%2 ==0){
+                       //se suman cada dos dias 
+                   }
+                   if (dias%3 ==0){
+                       //se suman cada tres dias 
+                   }
+                   // si las piezas necesarias para la computadora estan listas computadora = true y hilo emsabladores se activa
+
+
+                }
+            }
+            
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+   public static void main(String[] args) {
+        Semaphore semaforo= new Semaphore(3);
+       
+        Thread t =new Thread(new Main(semaforo,2));
+        Thread t1 =new Thread(new Main(semaforo,1));
+        //multithread
+        
+        t.start();
+   
+        System.out.println("Fuera del HIloooo");
+        
+    }
+   
+   /* public void run(){
+        System.out.println("En el Hilooo");
+       try {
+           sleep(100000000);
+       } catch (InterruptedException ex) {
+           Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+       }
+    }
+    */
     
 }
